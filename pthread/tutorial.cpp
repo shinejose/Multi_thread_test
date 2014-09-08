82bf334651394a5743c262ecb81359eb5a6c9243
shinejose <shinejose@163.com>
2014-09-08 22:45:46 +0800

    pthread tutorial 1

---
+++ pthread/tutorial.cpp
@@ -0,0 +1,63 @@
+// c header 
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <memory.h>
+// c++ header
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <utility>
+#include <iomanip>
+// c++11 header
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <thread>
+#include <chrono>
+#include <atomic>
+
+extern "C"
+{
+    #include <unistd.h> 
+}
+using namespace std;
+
+typedef long long  ll;
+typedef unsigned long long ull;
+
+void function1()
+{
+
+    cout << "hello..." << endl ;
+    sleep(2); // fall asLeep here for 2 seconds...
+
+}
+
+void function2()
+{
+    cout << " ....world" << endl ;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    function1();
+    function2();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
