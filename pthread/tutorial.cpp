86f4c92d7d1f77264614263da82a1bdd7e77b3b6
shinejose <shinejose@163.com>
2014-09-08 22:51:26 +0800

    pthread tutorial 2

--- pthread/tutorial.cpp
+++ pthread/tutorial.cpp
@@ -26,14 +26,15 @@
 
 extern "C"
 {
-    #include <unistd.h> 
+    #include <pthread.h>
+    #include <unistd.h>
 }
 using namespace std;
 
 typedef long long  ll;
 typedef unsigned long long ull;
 
-void function1()
+    void * function1(void *argument )
 {
 
     cout << "hello..." << endl ;
@@ -41,15 +42,22 @@ void function1()
 
 }
 
-void function2()
+    void * function2(void *argument)
 {
     cout << " ....world" << endl ;
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    function1();
-    function2();
+    // ios::sync_with_stdio(false);
+    pthread_t t1, t2 ; // declare 2 threads.
+    pthread_create( &t1, NULL, function1,NULL); // create a thread running function1
+    pthread_create( &t2, NULL, function2,NULL); // create a thread running function2
+
+    // Because all created threads are terminated when main() finishes, we have
+    // to give the threads some time to finish. Unfortunately for function1, main()
+    // will give only 1 second, but function1 needs at least 2 seconds. So function1 will
+    // probably be terminated before it can finish. This is a BAD way to manage threads.
+    sleep(1);    
     return 0;
 }
 
